47387fed66b8055a47a7886940d71a9631982630
Shebaz <multanishebaz@gmail.com>
2022-04-21 18:44:36 +0530

    Initial commit & include main class file

---
+++ any-cpt-listing-gutenberg-block.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Plugin Name: Any CPT Listing Gutenberg Block
+ * Description: Show list of any kind of wp post type including default posts as well as custom post types in front end  also with verity of settings.
+ * Version:     1.0.0
+ * Author:      Shebaz Multani
+ * Text Domain: any_cpt_listing_gutenberg_block
+ * Domain Path: /languages
+ */
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Main Class
+ */
+class AnyCPTListingGutenbergBlock
+{
+	
+	function __construct(argument)
+	{
+	
+	}
+
+}
+
